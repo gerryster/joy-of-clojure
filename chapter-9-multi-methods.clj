e4ddb3a09b7b3cff66168da94481e0348c7bea8a
Ryan Gerry <gerryster@gmail.com>
2014-04-27 12:25:36 -0500

    Chapter 9.

---
+++ chapter-9-multi-methods.clj
@@ -0,0 +1,26 @@
+(ns joy.udp
+  "The Universal Design Pattern (UDP) example from section 9.2."
+  (:refer-clojure :exclude [get]))
+
+(defn beget [o p] (assoc o ::prototype p))
+
+(def put assoc)
+
+(defn get [m k]
+  (when m
+    (if-let [[_ v] (find m k)]
+      v
+      (recur (::prototype m) k))))
+
+(def clone (partial beget {}))
+
+(defmulti compiler :os)
+(defmethod compiler ::unix [m] (get m :c-compiler))
+(defmethod compiler ::osx  [m] (get m :llvm-compiler))
+
+(def clone (partial beget {}))
+(def unix   {:os ::unix, :c-compiler "cc", :home "/home", :dev "/dev"})
+(def osx  (-> (clone unix)
+              (put :os ::osx)
+              (put :llvm-compiler "clang")
+              (put :home "/Users")))
